c846be3ccef35f7874b266a1b851f7b81a13bbfa
Adam Behrens <behrens.adam@gmail.com>
2016-06-21 22:07:24 -0700

    [feat] Hello World for loop

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main () {
+
+  for (int i = 1; i<=5; i++) {
+    cout << "Hello World + " << i << " ";
+  }
+
+  cout << endl;
+
+  return 0;
+}
